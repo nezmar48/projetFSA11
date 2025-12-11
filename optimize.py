1763eaeee420c30b9518b85696ff99abf648a129
nezmar48 <antoine@fraiteur.cz>
2025-12-11 20:10:54 +0100

    add tracker simulation

--- optimize.py
+++ optimize.py
@@ -13,14 +13,19 @@ a_x = lambda v, c: -c*abs(v)*v
 a_y = lambda v, c:-g-c*abs(v)*v
 
 #difference from simulaed and actual distance
-def dif(c, v0, x, angle):
+def dif(c, vx0, vy0, y0, x):
     xy = sm.simulation_xy(
-        sm.SimC(a = lambda v : a_x(v,c), v_0 = v0 * math.cos(angle)),
-        sm.SimC(a = lambda v : a_y(v,c), v_0 = v0 * math.sin(angle))
+        sm.SimC(a = lambda v : a_x(v,c), v_0 = vx0),
+        sm.SimC(a = lambda v : a_y(v,c), v_0 = vy0, x_0 = y0)
     )
     return xy[0][-1] - x
 
 # find the drag coefficient wich describes compleately the ball - size and mass included
+def find_c(vx0, vy0, y0, x_m):
+    # use brenteq to find zeros of a function (here the difference in distance as a function of coefficient)
+    return brentq(lambda c: dif(c, vx0, vy0, y0, x_m), 0, 100)
+
+# helper function for shell
 def find_c_shell():
     quit = False
     while not quit:
@@ -33,12 +38,15 @@ def find_c_shell():
             print("Input must be a float")
             quit = False
 
-    # use brenteq to find zeros of a function (here the difference in distance as a function of coefficient)
-    c_opt = brentq(lambda c: dif(c, v_mesured, x_mesured, angle), 0, 20)
+    vx0 = v_mesured * math.cos(angle)
+    vy0 = v_mesured * math.sin(angle)
+
+    c_opt = find_c(vx0, vy0, 0.6, x_mesured)
+
     print("The balls drag coefficient is: " + str(c_opt))
     xy_opt = sm.simulation_xy(
-        sm.SimC(a=lambda v : a_x(v,c_opt), v_0 = v_mesured * math.cos(angle)),
-        sm.SimC(a=lambda v : a_y(v,c_opt), v_0 = v_mesured * math.sin(angle))
+        sm.SimC(a=lambda v : a_x(v,c_opt), v_0 = vx0),
+        sm.SimC(a=lambda v : a_y(v,c_opt), v_0 = vy0, x_0 = 0.6)
     )
     plt.figure()
     plt.plot(xy_opt[0],xy_opt[1])
--- tracker.py
+++ tracker.py
@@ -4,28 +4,42 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import optimize
+import simulation as sm
 
 #one row of data
 class Mesure:
-    def __init__(self,temps,x,y,vitesseX,vitesseY):
-        self.temps = temps
-        self.x = x
-        self.y = y
-        self.vitesseX = vitesseX
-        self.vitesseY = vitesseY
+    def __init__(self, row):
+        self.temps = row[0]
+        self.x = row[1]
+        self.y = row[2]
+        self.vitesseX = row[3]
+        self.vitesseY = row[4]
 
 #full data table
 class Tableau_de_mesure:
     def __init__(self):
-        self.mesures=[]
         self.x = []
         self.y = []
         self.t = []
+        self.vx = []
+        self.vy = []
     def add_mesure(self,mesure):
-        self.mesures.append(mesure)
         self.x.append(mesure.x)
         self.y.append(mesure.y)
         self.t.append(mesure.temps)
+        self.vx.append(mesure.vitesseX)
+        self.vy.append(mesure.vitesseY)
+    #make ground level 0, breaks if simulation does not end at ground
+    def normalize(self):
+        x0 = self.x[0]
+        y0 = self.y[-1]  # ground at end of trajectory
+        self.x = [x - x0 for x in self.x]
+        self.y = [y - y0 for y in self.y]
+        if self.x[-1] < 0:
+            self.x = [-x for x in self.x]
+            self.vx = [-vx for vx in self.vx]
+
 
 #read data from file
 def read_tracker(file_name):
@@ -38,31 +52,39 @@ def read_tracker(file_name):
     with open(file_name, "r",) as f:
         #skip csv parameters
         f.readline()
-        #skip csv column names
+        #skip firs ligne
         f.readline()
         for ligne in f:
-            parts = ligne.split(",")
-            #ici nous allons traiter chaque mesure donnée dans le fichier
-            #on va stocker dans notre tableau de mesure les mesures
-            mesure = Mesure(float(parts[0].strip()),float(parts[1].strip()),float(parts[2].strip()),0,0)
-            table.add_mesure(mesure)
-    return table
+            row = []
+            try:
+                for p in ligne.replace(",",".").split(";"): #may be different in your file
+                    row.append(float(p.strip()))
+            except:
+                break
+            table.add_mesure(Mesure(row))
+    table.normalize()
+    return table 
 
 #plot data
 def plot_tracker(tables):
-    #on veut tracer un graphique avec les mesures
     plt.figure(1)
-    #on doit pouvoir récupérer les coordonnées
+    #find the simulation
+    t = tables[0]
+    #find drag constant for ball. If multiple different balls are used in the same graph this will fail.
+    c = optimize.find_c((t.vx[0] + t.vx[1])/2, (t.vy[0] + t.vy[1])/2, t.y[0], t.x[-1])
+    i = 1
     for table in tables:
-        print("nombre de mesures : ", len(table.mesures))
-        print(" les temps : " , table.t)
-        print(" les y : " , table.y)
-        plt.plot(table.t,table.y,
-            label="")
-    plt.xlabel("t [s]")
+        plt.plot(table.x,table.y, label = "mesurment " + str(i))
+        sim = sm.simulation_xy(
+            sm.SimC(a=lambda v : optimize.a_x(v,c), v_0 = (table.vx[0] + table.vx[1])/2),
+            sm.SimC(a=lambda v : optimize.a_y(v,c), v_0 = (table.vy[0] + table.vy[1])/2, x_0 = table.y[0])
+        )
+        plt.plot(sim[0],sim[1], label = "simulation " + str(i))
+        i += 1
+
+    plt.xlabel("x [m]")
     plt.ylabel("y [m]")
-    plt.legend(loc="upper right")
-    plt.tight_layout()
+    plt.legend()
     plt.show()
 
 #shell helper
